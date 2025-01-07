6a69799fff438ac37b6085f1aa3b84ee7d9d2cac
daehee719 <daeheuigang@gmail.com>
2025-01-07 17:01:31 +0900

    init

---
+++ social_project/src/main/java/com/one/social_project/SocialProjectApplication.java
@@ -0,0 +1,13 @@
+package com.one.social_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialProjectApplication.class, args);
+	}
+
+}
---
+++ social_project/src/test/java/com/one/social_project/SocialProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.social_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
