e6e916b32be4cc84734c9aa853ff191e48f23018
김준수 <asdg2795@naver.com>
2025-01-15 16:57:27 +0900

    fix : 불필요한 공백 정리

--- social_project/src/main/java/com/one/social_project/domain/chat/dto/ChatRoomType.java
+++ social_project/src/main/java/com/one/social_project/domain/chat/dto/ChatRoomType.java
@@ -1,6 +1,5 @@
 package com.one.social_project.domain.chat.dto;
 
-
 public enum ChatRoomType {
     DM, GM
 }
--- social_project/src/main/java/com/one/social_project/domain/chat/repository/ChatRoomRepository.java
+++ social_project/src/main/java/com/one/social_project/domain/chat/repository/ChatRoomRepository.java
@@ -1,6 +1,5 @@
 package com.one.social_project.domain.chat.repository;
 
-
 import com.one.social_project.domain.chat.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
