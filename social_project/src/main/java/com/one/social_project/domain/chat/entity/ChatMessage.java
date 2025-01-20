5a8e1e82222aab62f1e2f582c061d95fa6cdc9be
김준수 <asdg2795@naver.com>
2025-01-20 14:17:35 +0900

    refactor : MongoDB 스키마에서 ChatMessage 문서의 `sender`, `message`, `createdAt` 컬럼 제거"

--- social_project/src/main/java/com/one/social_project/domain/chat/entity/ChatMessage.java
+++ social_project/src/main/java/com/one/social_project/domain/chat/entity/ChatMessage.java
@@ -1,6 +1,5 @@
 package com.one.social_project.domain.chat.entity;
 
-import jakarta.persistence.Column;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -18,18 +17,13 @@ public class ChatMessage {
     @Id
     private String id; // MongoDB는 기본적으로 String ID를 사용합니다.
 
-    @Column(nullable = false)
     private String sender;      // 발신자
 
-    @Column(nullable = false)
     private String message;     // 메시지 내용
 
-    @Column(nullable = false)
     private LocalDateTime createdAt; // 메시지 생성 시간
 
     private String roomId; // 채팅방 ID (연관 관계 대신 필드로 관리)
 
     private List<String> readers; // 읽은 유저 목록
-
-
 }
