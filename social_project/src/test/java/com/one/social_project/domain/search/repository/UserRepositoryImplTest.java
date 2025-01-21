668cdaa196f2393392d5b3aab6594cce6b7d1908
eunoia73 <soo97703@naver.com>
2025-01-21 11:22:09 +0900

    test: #3 닉네임으로 유저 검색 테스트 수정

--- social_project/src/test/java/com/one/social_project/domain/search/repository/UserRepositoryImplTest.java
+++ social_project/src/test/java/com/one/social_project/domain/search/repository/UserRepositoryImplTest.java
@@ -1,26 +1,21 @@
 package com.one.social_project.domain.search.repository;
 
-import com.one.social_project.domain.search.dto.QUserSearchDTO;
+import com.one.social_project.domain.search.UserSearchCondition;
 import com.one.social_project.domain.search.dto.UserSearchDTO;
 import com.one.social_project.domain.user.entity.User;
-import com.querydsl.core.QueryResults;
-import com.querydsl.core.types.dsl.BooleanExpression;
+import com.one.social_project.domain.user.repository.UserRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-import static com.one.social_project.domain.user.entity.QUser.*;
 
 
 @SpringBootTest
@@ -31,6 +26,9 @@ class UserRepositoryImplTest {
     EntityManager em;
 
     JPAQueryFactory queryFactory;
+
+    @Autowired
+    private UserRepository userRepository;
 
 
     @BeforeEach
@@ -50,45 +48,19 @@ class UserRepositoryImplTest {
 
 
     @Test
+    @DisplayName("닉네임으로 유저 검색")
     public void selectByParam() {
-        String nickname = "u";
+        UserSearchCondition condition = new UserSearchCondition();
+        condition.setNickname("u");
+
         // 페이지 번호와 크기를 지정하여 Pageable 생성
         Pageable pageable = PageRequest.of(0, 4);
 
         // searchUser 메서드 호출
-        Page<UserSearchDTO> result = searchUser(nickname, pageable);
+        Page<UserSearchDTO> result = userRepository.searchUserByNickname(condition, pageable);
 
         Assertions.assertThat(result.getSize()).isEqualTo(4);
-        System.out.println("result" + result.get()); // 결과 출력
 
-    }
-
-    private Page<UserSearchDTO> searchUser(String nickname, Pageable pageable) {
-//        QUser users = user;
-        QueryResults<UserSearchDTO> result = queryFactory
-                .select(new QUserSearchDTO(
-                        user.id.as("userId"),
-                        user.nickname,
-                        user.email,
-                        user.profileImg
-                ))
-                .from(user)
-                .where(userNicknameLike(nickname))
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .fetchResults();
-
-        List<UserSearchDTO> content = result.getResults();
-        long total = result.getTotal();
-
-        return new PageImpl<>(content, pageable, total);
-    }
-
-    private BooleanExpression userNicknameLike(String userNicknameCond) {
-        if (userNicknameCond == null) {
-            return null;
-        }
-        return user.nickname.like("%" + userNicknameCond + "%");
     }
 
 }
