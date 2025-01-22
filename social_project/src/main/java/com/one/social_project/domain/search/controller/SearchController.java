4c9bb3d5039486830f88879e4e9e98160105ccfb
eunoia73 <soo97703@naver.com>
2025-01-22 16:18:43 +0900

    feature: #3 닉네임으로 유저 검색 페이징 수정

--- social_project/src/main/java/com/one/social_project/domain/search/controller/SearchController.java
+++ social_project/src/main/java/com/one/social_project/domain/search/controller/SearchController.java
@@ -2,17 +2,21 @@ package com.one.social_project.domain.search.controller;
 
 import com.one.social_project.domain.chat.entity.ChatMessage;
 import com.one.social_project.domain.search.ChatSearchCondition;
+import com.one.social_project.domain.search.PageableResponse;
 import com.one.social_project.domain.search.UserSearchCondition;
 import com.one.social_project.domain.search.dto.UserSearchDTO;
 import com.one.social_project.domain.search.service.SearchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -30,8 +34,24 @@ public class SearchController {
 
     //nickname으로 user 검색
     @GetMapping("/api/users/search")
-    public Page<UserSearchDTO> userSearchByNickname(UserSearchCondition condition, Pageable pageable) {
-        return chatSearchService.searchUserByNickname(condition, pageable);
+    public ResponseEntity<Map<String, Object>> userSearchByNickname(UserSearchCondition condition, Pageable pageable) {
+
+        Page<UserSearchDTO> result = chatSearchService.searchUserByNickname(condition, pageable);
+
+        // PageableResponse 객체 생성
+        PageableResponse pageableResponse = new PageableResponse(
+                result.getNumber(),
+                result.getSize(),
+                pageable.getOffset(),
+                result.isLast()
+        );
+
+        // Map으로 응답 구성
+        Map<String, Object> response = new HashMap<>();
+        response.put("content", result.getContent());
+        response.put("pageable", pageableResponse);
+
+        return ResponseEntity.ok(response);
     }
 
 
