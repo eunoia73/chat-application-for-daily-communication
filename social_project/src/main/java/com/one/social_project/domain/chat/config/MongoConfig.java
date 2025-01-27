d00714cf28aeab0cdbf60b38f96415876ea7c8fa
[코치] 최신범 <sinbum@kakao.com>
2025-01-27 16:30:50 +0900

    fix : mongoDB configration

--- social_project/src/main/java/com/one/social_project/domain/chat/config/MongoConfig.java
+++ social_project/src/main/java/com/one/social_project/domain/chat/config/MongoConfig.java
@@ -1,16 +1,17 @@
 package com.one.social_project.domain.chat.config;
 
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
 @Configuration
 public class MongoConfig {
 
-    @Value("spring.data.mongodb.uri")
+    @Value("${spring.data.mongodb.uri}")
     private String url;
 
     @Bean
